88bf89835b496d7a3dd96152426968aa8740ae3a
seulgi chu <seulgichu14@gmail.com>
2023-10-10 09:19:50 +0900

    [level unrated] Title: 부분 문자열인지 확인하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -1,7 +1,7 @@
 function solution(my_string, target) {
     let answer = 0;
 
-    for (let i = 0; i < my_string.length; i++) {
+    for (let i=0; i<my_string.length; i++) {
         const item = my_string.slice(i, i + target.length);
 
         if (item === target) answer = 1;
