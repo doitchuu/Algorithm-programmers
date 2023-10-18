adadc02646be1ea305504fde146d1ce262d97621
seulgi chu <seulgichu14@gmail.com>
2023-10-18 09:13:41 +0900

    [level unrated] Title: 홀수 vs 짝수, Time: 2.80 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -0,0 +1,23 @@
+function solution(num_list) {
+    var answer = 0;
+    let evenNum = 0;
+    let oddNum = 0;
+    
+    for (let i = 0; i < num_list.length; i++) {
+        if(i % 2 === 0) {
+            evenNum += num_list[i];
+          console.log(num_list[i]);
+        } else {
+            oddNum += num_list[i];
+            console.log(num_list[i]);
+          }
+    }
+        
+    if (evenNum > oddNum) {
+        answer = evenNum;
+    } else {
+        answer = oddNum;
+    }
+        
+    return answer;
+}
