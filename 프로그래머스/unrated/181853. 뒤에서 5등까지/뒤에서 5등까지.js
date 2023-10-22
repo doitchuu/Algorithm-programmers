ce08ab5a2283a3d6be8894364a666bd2d35b78de
seulgi chu <seulgichu14@gmail.com>
2023-10-23 07:12:38 +0900

    [level unrated] Title: 뒤에서 5등까지, Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181853. 뒤에서 5등까지/뒤에서 5등까지.js
@@ -0,0 +1,12 @@
+function solution(num_list) {
+    var answer = [];
+    
+    num_list.sort(function (a, b) {
+        return a - b;
+    });
+    
+    for (let i = 0; i < 5; i++) {
+        answer.push(num_list[i]);
+    }
+    return answer;
+}
