c39a4bfd50e7904e7c07cb3d104dc25eefbc9cd9
seulgi chu <seulgichu14@gmail.com>
2023-10-27 06:56:58 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.39 ms, Memory: 37.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    var answer = [];
+    for (let i = 0; i < n; i++) {
+        answer.push([]);
+        
+        for (let j = 0; j < n; j++) {
+            if (i === j) {
+                answer[i].push(1);
+            } else {
+              answer[i].push(0);
+            }
+        }
+    }
+  
+    return answer;
+}
