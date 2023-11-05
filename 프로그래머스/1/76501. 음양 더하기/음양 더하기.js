0af038fd2ca43b3fded4d23f27d8f333a2dacabd
seulgi chu <seulgichu14@gmail.com>
2023-11-06 08:17:17 +0900

    [level 1] Title: 음양 더하기, Time: 0.15 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.js
@@ -0,0 +1,12 @@
+function solution(absolutes, signs) {
+    let answer = 0;
+    
+    for (let i = 0; i < signs.length; i++) {
+        if (signs[i] === false) {
+            answer += absolutes[i] * -1;
+        } else {
+            answer += absolutes[i];
+        }
+    }
+    return answer;
+}
