1896c56070ea00e69b65df0d4d7cf6d81ac39731
seulgi chu <seulgichu14@gmail.com>
2023-11-12 12:39:22 +0900

    [level 1] Title: 나누어 떨어지는 숫자 배열, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12910. 나누어 떨어지는 숫자 배열/나누어 떨어지는 숫자 배열.js
@@ -0,0 +1,17 @@
+function solution(arr, divisor) {
+    var answer = [];
+    
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] % divisor === 0) {
+            answer.push(arr[i]);
+        }
+    }
+    
+    if (!answer.length) {
+        answer.push(-1);
+    }
+    
+    answer.sort((a, b) => a - b)
+    
+    return answer;
+}
