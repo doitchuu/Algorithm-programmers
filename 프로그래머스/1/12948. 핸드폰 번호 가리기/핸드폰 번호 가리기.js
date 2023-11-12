31d9db9a237c873bd76d6d05a7cb506b5c1e3bb2
seulgi chu <seulgichu14@gmail.com>
2023-11-12 14:06:53 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,6 +1,5 @@
 function solution(phone_number) {
     let answer = "";
-    const lastIndex = phone_number.length - 1;
     
     for (let i = 0; i < phone_number.length; i++) {
         if (i > phone_number.length - 5) {
