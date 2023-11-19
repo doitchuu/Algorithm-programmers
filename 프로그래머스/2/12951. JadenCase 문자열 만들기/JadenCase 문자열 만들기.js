3ff30705a3a013fd2cdbd2029786198dfd904b03
seulgi chu <seulgichu14@gmail.com>
2023-11-20 08:00:59 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,11 +1,13 @@
 function solution(s) {
     let answer = "";
+    
     for (let i = 0; i < s.length; i++) {
-      if (i === 0 || s[i-1] === " ") {
-        answer += s[i].toUpperCase();
-      } else {
-        answer += s[i].toLowerCase();
-      }
+        if (i === 0 || s[i - 1] === " ") {
+            answer += s[i].toUpperCase();
+        } else {
+            answer += s[i].toLowerCase();
+        }
     }
+    
     return answer;
 }
