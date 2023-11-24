52e0cf7521264a0b4cc06aca8eeda57637b20738
seulgi chu <seulgichu14@gmail.com>
2023-11-24 14:07:39 +0900

    [level 2] Title: 올바른 괄호, Time: 6.08 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -0,0 +1,13 @@
+function solution(s) {
+  const stack = [];
+  
+  s.split("").map(string => {
+    if (string === ")" && stack[stack.length - 1] === "(") {
+      stack.pop();
+    } else {
+      stack.push(string);
+    }
+  });
+  
+  return stack.length === 0;
+}
