075df2d904178306937c86abf8740ce25ba221a2
seulgi chu <seulgichu14@gmail.com>
2024-05-09 23:58:36 +0900

    [level 0] Title: 등수 매기기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120882. 등수 매기기/README.md
+++ 프로그래머스/0/120882. 등수 매기기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 등수 매기기 - 120882 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120882#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120882) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.14 ms
+메모리: 33.4 MB, 시간: 0.15 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 09일 23:56:18
+2024년 05월 09일 23:58:34
 
 ### 문제 설명
 
--- 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
+++ 프로그래머스/0/120882. 등수 매기기/등수 매기기.js
@@ -1,12 +1,12 @@
 function solution(score) {
     const answer = [];
     let ranking = 1;
-    
+
     for (let i = 0; i < score.length; i++) {
         for(let j = 0; j < score.length; j++) { 
             if ((score[i][0] + score[i][1]) < (score[j][0] + score[j][1])) {
                 ranking++;
-            } else if ((score[i][0] + score[i][1]) == (score[j][0] + score[j][1])) {
+            } else if ((score[i][0] + score[i][1]) === (score[j][0] + score[j][1])) {
                 continue;
             } 
         } 
