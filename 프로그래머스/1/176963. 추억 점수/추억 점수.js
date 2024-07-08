73e96877fb7e0bd5ccc254f29440c176d12667bf
seulgi chu <seulgichu14@gmail.com>
2024-07-09 00:03:13 +0900

    [level 1] Title: 추억 점수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.js
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.js
@@ -1,21 +1,28 @@
 function solution(name, yearning, photo) {
-    let result = [];
-    const personYearning = {};
-    
-    for (let i = 0; i < name.length; i++) {
-        personYearning[name[i]] = yearning[i];
-    }
-    
-    for (let i=0; i<photo.length; i++) {
-    let photoArr = [];    
-      for (let j=0; j<photo[i].length; j++) {
-          photoArr.push(photo[i][j])
-      }
-        let photoYearningSum = 0;
-        for (let k=0; k<photoArr.length; k++) {
-            if (personYearning[photoArr[k]]) photoYearningSum += personYearning[photoArr[k]]
-        }
-            result.push(photoYearningSum)
+  let result = [];
+  const personYearning = {};
+
+  for (let i = 0; i < name.length; i++) {
+    personYearning[name[i]] = yearning[i];
   }
-    return result;
+
+  for (let i = 0; i < photo.length; i++) {
+    let photoList = [];
+
+    for (let j = 0; j < photo[i].length; j++) {
+      photoList.push(photo[i][j]);
+    }
+
+    let photoYearningSum = 0;
+
+    for (let k = 0; k < photoList.length; k++) {
+      if (personYearning[photoList[k]]) {
+        photoYearningSum += personYearning[photoList[k]];
+      }
+    }
+
+    result.push(photoYearningSum);
+  }
+
+  return result;
 }
