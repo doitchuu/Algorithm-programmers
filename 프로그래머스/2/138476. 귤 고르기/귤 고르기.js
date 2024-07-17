3ae6c6ade9a0bcc8a31751c3698924657aa8ffa9
seulgi chu <seulgichu14@gmail.com>
2024-07-17 19:00:10 +0900

    [level 2] Title: 귤 고르기, Time: 110.71 ms, Memory: 64.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -0,0 +1,28 @@
+function solution(k, tangerine) {
+  const tangerineList = {};
+  
+  for (let i = 0; i < tangerine.length; i++) {
+    if (!tangerineList[tangerine[i]]) {
+      tangerineList[tangerine[i]] = 1;
+      continue;
+    } 
+    
+    tangerineList[tangerine[i]] += 1;
+  }
+  
+  const sortedCounts = Object.values(tangerineList).sort((a, b) => b - a);
+  let numberCount = 0;
+  let total = 0;
+  
+  console.log(sortedCounts)
+  for (let count of sortedCounts) {
+    total += count;
+    numberCount++;
+    
+    if (total >= k) {
+      break;
+    }
+  }
+  
+  return numberCount;
+}
