cbf6704002555940f027609883de7389adcbef24
seulgi chu <seulgichu14@gmail.com>
2024-10-09 15:43:23 +0900

    [level 2] Title: 방문 길이, Time: 2.57 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.js
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.js
@@ -1,59 +1,54 @@
-const MINIMUM = -5;
-const MAXIMUM = 5;
-
-function canGo(number) {
-  if (number < MINIMUM || number > MAXIMUM) {
-    return false;
-  }
-    
-  return true;
-}
+const MIN_NUMBER = -5;
+const MAX_NUMBER = 5;
 
 function solution(dirs) {
-  const history = {};
-  const current = [0, 0];
-  let firstVisitcount = 0;
-    
-  for (const command of dirs) {
-    move(current, command);
+  let current = [0, 0];
+  const roadResult = {};
+  let visitCount = { count: 0 };
+  
+  for (let i = 0; i < dirs.length; i++) {
+    current = findCoordinate(dirs[i], current, roadResult, visitCount);
   }
-    
-  return firstVisitcount;
-    
-  function move(current, command) {
-    const next = [...current];
+  
+  return visitCount.count;
+}
+
+function findCoordinate(dir, current, roadResult, visitCount) {
+  const next = [...current];
+  
+  switch(dir) {
+    case "U":
+      next[1] += 1;
+      break;
       
-    switch(command) {
-      case "U":
-        next[1] += 1;
-        break;
-            
-      case "D":
-        next[1] -= 1;
-        break;
-            
-      case "L":
-        next[0] -= 1;
-        break;
-            
-      case "R":
-        next[0] += 1;
-        break;
-    }
+    case "D":
+      next[1] -= 1;
+      break;
       
-    const [x, y] = next;
+    case "L":
+      next[0] -= 1;
+      break;
       
-    if (!canGo(x) || !canGo(y)) {
-      return;
-    }
-      
-    if (!history[`${current} to ${next}`]) {
-      firstVisitcount++;
-      history[`${current} to ${next}`] = true;
-      history[`${next} to ${current}`] = true;
-    }
-      
-    current[0] = x;
-    current[1] = y;
+    case "R":
+      next[0] += 1;
+      break;  
   }
+  
+  if (
+    next[0] < MIN_NUMBER ||
+    next[1] < MIN_NUMBER ||
+    next[0] > MAX_NUMBER ||
+    next[1] > MAX_NUMBER
+  ) {
+    return current;
+  }
+  
+   if (!roadResult[`${current} to ${next}`]) {
+    visitCount.count++;
+  }
+  
+  roadResult[`${current} to ${next}`] = true;
+  roadResult[`${next} to ${current}`] = true;
+  
+  return next;
 }
