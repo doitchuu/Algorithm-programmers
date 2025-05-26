94f9d76a52e511f3f44186752937e6256515f4b1
doitchuu <seulgichu14@gmail.com>
2025-05-26 13:30:24 +0900

    [level 1] Title: 모의고사, Time: 7.31 ms, Memory: 36 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,25 +1,37 @@
 function solution(answers) {
-  let answer = [];
-  const oneSupo = [1, 2, 3, 4, 5];
-  const twoSupo = [2, 1, 2, 3, 2, 4, 2, 5];
-  const threeSupo = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
-
-  const oneSupoScore = answers.filter((value, index) => { return value === oneSupo[index % oneSupo.length] }).length;
-  const twoSupoScore = answers.filter((value, index) => { return value === twoSupo[index % twoSupo.length] }).length;
-  const threeSupoScore = answers.filter((value, index) => { return value === threeSupo[index % threeSupo.length] }).length;
+  const result = [];
+  const people = [0,0,0];
+  const length = answers.length;
   
-  const maxScore = Math.max(oneSupoScore, twoSupoScore, threeSupoScore);
-
-  if (oneSupoScore === maxScore) {
-    answer.push(1);
+  const person1 = fitArrayLength([1, 2, 3, 4, 5], length);
+  const person2 = fitArrayLength([2, 1, 2, 3, 2, 4, 2, 5], length);
+  const person3 = fitArrayLength([3, 3, 1, 1, 2, 2, 4, 4, 5, 5], length);
+  
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === person1[i]) {
+      people[0]++;
+    }
+    
+    if (answers[i] === person2[i]) {
+      people[1]++;
+    }
+    
+    if (answers[i] === person3[i]) {
+      people[2]++;
+    }
   }
   
-  if (twoSupoScore === maxScore) { 
-    answer.push(2);
+  const maxResult = Math.max(...people);
+  
+  for (let i = 0; i < people.length; i++) {
+    if (people[i] === maxResult) {
+      result.push(i + 1);
+    }
   }
-  if (threeSupoScore === maxScore) { 
-    answer.push(3);
-  }
-
-  return answer;
+  
+  return result;
+}
+  
+function fitArrayLength (array, length) {
+  return Array.from({ length }, (_, i) => array[i % array.length]);
 }
