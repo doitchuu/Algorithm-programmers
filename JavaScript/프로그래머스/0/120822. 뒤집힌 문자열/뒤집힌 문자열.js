231134e624145bf745e212b7da5c8ba3a8e70758
doitchuu <seulgichu14@gmail.com>
2025-08-04 22:41:28 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    let result = "";
+    
+    for (let i = 1; i <= my_string.length; i++) {
+        result += my_string[my_string.length - i];
+    }
+    
+    return result;
+}
