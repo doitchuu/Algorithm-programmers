9e0f9a07785efedfc4d79128f468f62e1b2f9bd6
doitchuu <seulgichu14@gmail.com>
2025-08-13 00:00:35 +0900

    [level 0] Title: 암호 해독, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120892. 암호 해독/암호 해독.js
@@ -0,0 +1,9 @@
+function solution(cipher, code) {
+    let result = "";
+    
+    for (let i = code - 1; i < cipher.length; i = i + code) {
+        result += cipher[i];
+    }
+    
+    return result;
+}
