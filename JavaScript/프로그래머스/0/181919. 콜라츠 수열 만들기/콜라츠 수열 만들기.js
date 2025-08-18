04f7c76fac7fcbc4823ba12393b3e48577f546ff
doitchuu <seulgichu14@gmail.com>
2025-08-18 20:50:51 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.js
@@ -0,0 +1,16 @@
+function solution(n) {
+    const answer = [n];
+    let num = n;
+
+    while (num > 1) {
+        if (num % 2 === 0) {
+            num = num / 2;
+        } else {
+            num = 3 * num + 1;
+        }
+        
+        answer.push(num);
+    }
+    
+    return answer;
+}
