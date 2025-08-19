837ab322f15fb956a74420459d0e51be6fbb111b
doitchuu <seulgichu14@gmail.com>
2025-08-20 00:07:15 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 67.88 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.js
@@ -0,0 +1,17 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    const firstNum = Number(input[0]);
+    const secondNum = Number(input[1]);
+    const result = firstNum + secondNum;
+    
+    console.log(`${firstNum} + ${secondNum} = ${result}`);
+});
