a58ed149cac1718d4262fc2388de2a11ce3068a8
doitchuu <seulgichu14@gmail.com>
2025-08-27 13:06:34 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181886. 5명씩/5명씩.js
+++ JavaScript/프로그래머스/0/181886. 5명씩/5명씩.js
@@ -1,9 +1,3 @@
 function solution(names) {
-    const answer = [];
-    
-    for (let i = 0; i < names.length; i += 5) {
-        answer.push(names[i]);
-    }
-    
-    return answer;
+    return names.filter((name, index) => index % 5 === 0);
 }
