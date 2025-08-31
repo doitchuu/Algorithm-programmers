21985fb0a428010ecf0f43028e119ece00834f3f
doitchuu <seulgichu14@gmail.com>
2025-08-31 17:38:40 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,11 @@
+function solution(numbers) {
+    let sum = 0;
+    
+    for (let i = 0; i < 10; i++) {
+        if (numbers.indexOf(i) === -1) {
+            sum += i;
+        }
+    }
+    
+    return sum;
+}
