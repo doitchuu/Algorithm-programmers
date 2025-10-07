5f3f388d6da4740d5b4790a18a42536b60557d52
doitchuu <seulgichu14@gmail.com>
2025-10-07 23:06:08 +0900

    [level 0] Title: 소인수분해, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120852. 소인수분해/소인수분해.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  let answer = new Set();
+
+  for (let i = 2; i <= n; i++) {
+    while (n % i === 0) {
+      answer.add(i);
+      n /= i;
+    }
+  }
+
+  return [...answer];
+}
