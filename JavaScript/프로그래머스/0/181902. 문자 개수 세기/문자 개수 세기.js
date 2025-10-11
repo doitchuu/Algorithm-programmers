5ab4d231c4da2eff6c700025065071336c38e8a3
doitchuu <seulgichu14@gmail.com>
2025-10-11 23:16:11 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.43 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.js
@@ -0,0 +1,15 @@
+function solution(my_string) {
+  const counts = Array(52).fill(0);
+
+  for (let ch of my_string) {
+    const code = ch.charCodeAt(0);
+
+    if (code >= 65 && code <= 90) {
+      counts[code - 65]++;
+    } else if (code >= 97 && code <= 122) {
+      counts[code - 71]++;
+    }
+  }
+
+  return counts;
+}
