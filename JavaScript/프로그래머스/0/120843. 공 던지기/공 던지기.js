f28f42686c958b949546c82e16d9a4b15513eba4
doitchuu <seulgichu14@gmail.com>
2025-10-15 01:04:37 +0900

    [level 0] Title: 공 던지기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/0/120843. 공 던지기/공 던지기.js
@@ -0,0 +1,9 @@
+function solution(numbers, k) {
+    let index = 0;
+    
+    for (let i = 1; i < k; i++) {
+        index = (index + 2) % numbers.length;
+    }
+    
+    return numbers[index];
+}
