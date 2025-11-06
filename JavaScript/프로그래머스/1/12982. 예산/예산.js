05b4929489d18a0b8a13521f1eb02526499643fe
doitchuu <seulgichu14@gmail.com>
2025-11-06 23:41:57 +0900

    [level 1] Title: 예산, Time: 0.16 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ JavaScript/프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,17 @@
+function solution(d, budget) {
+  d.sort((a, b) => a - b);
+  
+  let count = 0;
+  let sum = 0;
+
+  for (let i = 0; i < d.length; i++) {
+    sum += d[i];
+    if (sum <= budget) {
+      count++;
+    } else {
+      break;
+    }
+  }
+
+  return count;
+}
