4dc77b602cc60bc67f73c81dfb2011b504ec0c52
doitchuu <seulgichu14@gmail.com>
2025-11-18 23:42:42 +0900

    [level 2] Title: 숫자의 표현, Time: 0.36 ms, Memory: 33 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ JavaScript/프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,31 +1,7 @@
 function solution(n) {
-    if (n === 1 || n === 2 || n === 4) {
-        return 1;
-    }
-    
     let count = 0;
-    let number = 1;
-    
-    while (number !== n) {
-        let sum = 0;
-        
-        for (let i = number; i < n; i++) {
-            if (sum < n) {
-                sum += i;
-            }
-            
-            if (sum === n) {
-                count++;
-                break;
-            }
-            
-            if (sum > n) {
-                break;
-            }
-        }
-        
-        number++;
+    for (let i = 1; i <= n; i++) {
+        if (n % i === 0 && i % 2 === 1) count++;
     }
-    
-    return count + 1;
+    return count;
 }
