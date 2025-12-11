47b335f172e112226dcf0778e463019263f1e637
doitchuu <seulgichu14@gmail.com>
2025-12-11 23:53:39 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ JavaScript/프로그래머스/0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,10 +1,14 @@
 function solution(common) {
-    const multiply = common[1] / common[0];
+    const multiply = common[1] / common[0]; 
     const sum = common[1] - common[0];
-
-    if (common[1] / common[0] === common[2] / common[1]) {
-        return common[common.length - 1] * multiply;
-    } else {
-        return common[common.length - 1] + sum;
+    
+    for (let i = 0; i < common.length; i++) {
+        if (common[i] * multiply !== common[i + 1]) {
+            return common[common.length - 1] + sum;
+        }
+        
+        if (common[i] + sum !== common[i + 1]) {
+            return common[common.length - 1] * sum;
+        }
     }
 }
