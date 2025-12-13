ff5accc0b13b8dabf5ad0516f7f5336fafb5fa2f
doitchuu <seulgichu14@gmail.com>
2025-12-13 16:39:13 +0900

    [level 2] Title: 행렬의 곱셈, Time: 9.96 ms, Memory: 38.7 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
+++ JavaScript/프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.js
@@ -1,9 +1,6 @@
 function solution(arr1, arr2) {
     var answer = [];
     
-    // arr1의 첫 행과 arr2의 열들(0, arr2.length - 1)을 곱해서 배열에 값을 구해서 넣는다.
-    // arr1.length - 1까지 행렬을 곱해준다.
-    // arr1[i][j] * arr2[j][i] + arr1[i][j] * arr
     for (let i = 0; i < arr1.length; i++) {
         const result = [];
         
