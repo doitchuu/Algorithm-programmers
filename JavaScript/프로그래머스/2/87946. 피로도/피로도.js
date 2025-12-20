fea55303d1eb70a28317c09b35ad85440ade0952
doitchuu <seulgichu14@gmail.com>
2025-12-20 17:26:03 +0900

    [level 2] Title: 피로도, Time: 8.95 ms, Memory: 38.2 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/87946. 피로도/피로도.js
+++ JavaScript/프로그래머스/2/87946. 피로도/피로도.js
@@ -1,4 +1,33 @@
 function solution(k, dungeons) {
-    var answer = -1;
-    return answer;
+    const isUsed = new Array(dungeons.length).fill(false);
+    let result = 0;
+    
+    function dfs(hp, depth) {
+        if (dungeons.length === depth) {
+            return;
+        }
+    
+        for (let start = 0; start < dungeons.length; start++) {
+            const [need, fatigue] = dungeons[start];
+            
+            if (isUsed[start]) {
+                continue;
+            }
+            
+            if (hp < need || fatigue > hp || hp <= 0) {
+                continue;
+            }
+            
+            isUsed[start] = true;
+            result = Math.max(result, depth + 1);
+            
+            dfs(hp - fatigue, depth + 1);
+            
+            isUsed[start] = false;
+        }
+    }
+    
+    dfs(k, 0);
+    
+    return result;
 }
