2504457e93c04247160a27fed708a8010dddb9b4
doitchuu <seulgichu14@gmail.com>
2026-01-07 23:53:00 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ JavaScript/프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,13 +1,14 @@
 function solution(n) {
-    const dp = [0, 1, 2];
-    
     if (n === 1 || n === 2) {
-        return dp[n];
+        return n;
     }
     
-    for (let i = 3; i <= n; i++) {
-        dp[i] = (dp[i - 2] + dp[i - 1]) % 1234567;
+    const dp = [1, 2];
+    const mov = 1234567;
+    
+    for (let i = 2; i < n; i++) {
+        dp[i] = (dp[i - 2] + dp[i - 1]) % mov;
     }
     
-    return dp[n];
+    return dp[n - 1];
 }
