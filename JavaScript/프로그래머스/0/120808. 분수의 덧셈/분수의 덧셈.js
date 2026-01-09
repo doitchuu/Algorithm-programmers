4e62f0b9996a9b710125b1a640b9aeb6b68f71f1
doitchuu <seulgichu14@gmail.com>
2026-01-10 03:41:11 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ JavaScript/프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,12 +1,16 @@
 function solution(numer1, denom1, numer2, denom2) {
     let numerator = numer1 * denom2 + numer2 * denom1;
     let denominator = denom1 * denom2;
-
-    const g = gcd(numerator, denominator);
-    return [numerator / g, denominator / g];
+    
+    const number = gcb(numerator, denominator);
+    
+    return [numerator / number, denominator / number];
 }
 
-function gcd(a, b) {
-    if (b === 0) return a;
-    return gcd(b, a % b);
+function gcb(a, b) {
+    if (b === 0) {
+        return a;
+    }
+    
+    return gcb(b, a % b);
 }
