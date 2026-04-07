196a8ade47b15e46e6334fc98d881f05847bb2b8
doitchuu <seulgichu14@gmail.com>
2026-04-07 11:49:31 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.13 ms, Memory: 33.3 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ JavaScript/프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,4 +1,4 @@
 function solution(s) {
-    const strList = s.split(" ").sort((a, b) => a - b);
-    return strList[0] + " " + strList[strList.length - 1];
+    const numList = s.split(" ").sort((a, b) => a - b);  
+    return numList[0] + " " + numList[numList.length - 1];
 }
